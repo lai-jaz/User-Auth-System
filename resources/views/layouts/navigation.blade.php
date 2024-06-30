7076366f7b343109f229eac482d2d97210858c33
Laiba Ijaz <125128708+lai-jaz@users.noreply.github.com>
2024-07-01 01:00:05 +0500

    modified:   resources/views/admin-dashboard.blade.php 	modified:   resources/views/layouts/navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -14,7 +14,7 @@
                 
                     <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
 
-                    <x-nav-link :href="url('/')" :active="request()->routeIs('admin-dashboard')">
+                    <x-nav-link :href="url('/')">
                         {{ __('Home') }}
                     </x-nav-link>
 
